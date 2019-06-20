25929c9a4727b6e9afc6a1a31516fda0ffcb61ce
Manav Sethi <dev.manav.sethi@gmail.com>
2019-06-21 03:28:18 +0530

    grpc edition

--- controllers/HTTPhandlers.go
+++ controllers/HTTPhandlers.go
@@ -10,3 +10,26 @@ func Home(HTTPresponseWriter http.ResponseWriter, HTTPrequest *http.Request) {
 	fmt.Fprintln(HTTPresponseWriter, "Welcome to Home Automation Server!")
 
 }
+
+func App(HTTPresponseWriter http.ResponseWriter, HTTPrequest *http.Request) {
+
+	Commands, ok := HTTPrequest.URL.Query()["command"]
+
+	if !ok || len(Commands[0]) < 1 {
+
+		return
+	}
+
+	switch Commands[0] {
+
+	case "CreateClient":
+
+		break
+
+	case "EditClient":
+
+		break
+
+	}
+
+}
--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"net/http"
-	"os"
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/Dev-ManavSethi/Home-Automation-Server/services"
 
 	"github.com/Dev-ManavSethi/Home-Automation-Server/utils"
+	"google.golang.org/grpc"
 
 	"github.com/joho/godotenv"
-
-	"github.com/Dev-ManavSethi/Home-Automation-Server/controllers"
 )
 
 type Student struct {
@@ -17,6 +19,8 @@ type Student struct {
 	Class string
 }
 
+type server struct{}
+
 func init() {
 
 	err := godotenv.Load(".env")
@@ -24,11 +28,24 @@ func init() {
 
 }
 
+func (*server) CreateClient(ctx context.Context, CreateClientRequest *services.CreateClientRequest) (*services.CreateClientResponse, error) {
+
+	return nil, nil
+}
+
 func main() {
 
-	http.HandleFunc("/", controllers.Home)
+	fmt.Println("Home Automation Server")
 
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	utils.LogErrorOrSuccess(err, "Error starting HTTP server at port: "+os.Getenv("PORT"), "HTTP server listening at port: "+os.Getenv("PORT"))
+	Listener, err := net.Listen("tcp", "0.0.0.0:50051")
+	utils.LogErrorOrSuccess(err, "Error listening at 0.0.0.0:50051", "gRPC server listening at 0.0.0.0:50051")
+
+	Server := grpc.NewServer()
+	services.RegisterControlHomeServer(Server, &server{})
+
+	// Register reflection service on gRPC server.
+
+	err02 := Server.Serve(Listener)
+	utils.LogErrorOrSuccess(err02, "Error to serve the listener", "Successfully serving the Lister at 0.0.0.0:50051")
 
 }
